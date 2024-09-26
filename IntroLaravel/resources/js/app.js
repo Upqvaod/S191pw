ef6944f6c09bf974efa15dea5d39ff565cf43871
DiegoV.O <122044299@upq.edu.mx>
2024-09-25 20:33:23 -0600

    Avance de clase

--- IntroLaravel/resources/js/app.js
+++ IntroLaravel/resources/js/app.js
@@ -1,2 +1,3 @@
 import './bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+/*el primer parametro es la vista y el segundo es la ruta.
+Funciona tanto la sintaxis de arriba como la de abajo*/
+Route::view('/','welcome');
+
+Route::view('/formulario','formulario');
